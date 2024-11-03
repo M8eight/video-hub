7899699da1fb8ae5501831a547fc519ab2960bc0
M8eight <xui xui>
2024-11-03 19:31:58 +0300

    merge

---
+++ videohub-frontend/src/slices/report/reportRequests.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { request } from "../../helpers/axios_helper";
+
+const getReportsRequest = async ({offset, limit}) => {
+    return await request("get", `/api/report/all?offset=${offset}&limit=${limit}`).then((res) => res.data);
+}
+
+export const getReports = createAsyncThunk(
+    "report/getReports",
+    async ({offset, limit}) => {
+        return await getReportsRequest({offset, limit})
+    }
+)
+
+export const getMoreReports = createAsyncThunk(
+    "report/getMoreReports",
+    async ({offset, limit}) => {
+        return await getReportsRequest({offset, limit})
+    }
+)
+
+export const addReport = createAsyncThunk(
+    "report/addReport",
+    async (reportForm) => {
+        return await request("post", "/api/report", reportForm, { "Content-Type": "multipart/form-data" }).then((response) => response.data);
+    }
+);
